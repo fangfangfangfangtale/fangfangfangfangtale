4999befa5ee2110e2ff5f50811a01ba2936fd3e4
fangfangfangfangtale <53353252+fangfangfangfangtale@users.noreply.github.com>
2019-08-08 18:04:08 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char name[40] = "yanika udommark";
+    int distance;
+    float velocity;
+    int hour,min,sec;
+    float x;
+
+
+    printf("Input your name :");
+    scanf("%[^\n]s",name);
+
+    printf("Input distance (KM) : ");
+    scanf("%d",distance);
+
+    printf("Input avg velocity (KM/H) : ");
+    scanf("%f",velocity);
+
+    printf("My name is %s \n",name);
+    printf("Distance between house and school : %.2f KMs\n",(float)distance);
+    printf("I walk by velocity (KM / Hour) : %.2f \n",velocity);
+
+    hour = distance/velocity;
+    x = (float)distance/velocity;
+    x = x - hour;
+    x = x*60;
+    min = (int)x;
+    x = x-min;
+    sec = x*60;
+
+    printf("Time from home to school : %d Hours,%d mins,%d secs",hour,min,sec);
+
+    return 0;
+}
